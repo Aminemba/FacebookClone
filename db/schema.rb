75a45ff282784776d15d63b9942b065ee457e95c
amin.mba@iuj.ac.jp <amin.mba@iuj.ac.jp>
2020-09-04 10:29:00 +0000

    Create Friendship Model , Add Index to Post and Friendship , migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_04_102404) do
+ActiveRecord::Schema.define(version: 2020_09_04_102655) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,17 @@ ActiveRecord::Schema.define(version: 2020_09_04_102404) do
     t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id", "created_at"], name: "index_comments_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "friendships", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "friend_id", null: false
+    t.boolean "friendship_status"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["friend_id"], name: "index_friendships_on_friend_id"
+    t.index ["friendship_status"], name: "index_friendships_on_friendship_status"
+    t.index ["user_id"], name: "index_friendships_on_user_id"
   end
 
   create_table "likes", force: :cascade do |t|
@@ -44,6 +55,7 @@ ActiveRecord::Schema.define(version: 2020_09_04_102404) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["author", "created_at"], name: "index_posts_on_author_and_created_at"
+    t.index ["author"], name: "index_posts_on_author"
   end
 
   create_table "users", force: :cascade do |t|
@@ -61,4 +73,6 @@ ActiveRecord::Schema.define(version: 2020_09_04_102404) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "friendships", "users"
+  add_foreign_key "friendships", "users", column: "friend_id"
 end
