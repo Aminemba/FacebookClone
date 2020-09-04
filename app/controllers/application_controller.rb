a4506c1c455c9e506727c3d823421954748de8c7
amin.mba@iuj.ac.jp <amin.mba@iuj.ac.jp>
2020-09-04 10:36:36 +0000

    Configure and define the relationships btw the user , posts , friendships , like and likeable models , configure the app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
-end
+  before_action :authenticate_user!
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    helper_method :friends_and_myself
+
+
+    private
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: %i[name lastname])
+      devise_parameter_sanitizer.permit(:account_update, keys: %i[name lastname])
+    end
+  end
